3dc2d84aed8b46505b0859c1306d63d9a25e327c
Beau Gunderson <beau@beaugunderson.com>
2013-06-18 15:07:22 -0700

    Update for Twitter API 1.1, add mysql database

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var async = require('async');
 var elongate = require('elongate');
+var mysql = require('mysql');
 var request = require('request');
 
 var http = require('http');
@@ -8,51 +9,74 @@ var https = require('https');
 http.globalAgent.maxSockets = 100;
 https.globalAgent.maxSockets = 100;
 
-var BASE_URL = 'http://search.twitter.com/search.json';
+var db = mysql.createConnection({
+  host: 'localhost',
+  user: 'quantified_qs',
+  database: 'quantified_qs',
+  password: process.env.MYSQL_PASSWORD
+});
+
+var BASE_URL = 'https://api.twitter.com/1.1/search/tweets.json';
 
 elongate = async.memoize(elongate);
 
-var stats = {};
-
 function getPage(qs) {
-  console.log('# Fetching', (qs || 'first page'));
+  console.log('Fetching', (qs || 'first page'));
 
   request.get({
     url: BASE_URL + (qs ||
-      '?include_entities=true&q=%40quantifiedself&count=50&result_type=recent&rpp=50'),
-    json: true
+      '?include_entities=true&q=%40quantifiedself&count=100&result_type=recent&rpp=100'),
+    json: true,
+    headers: {
+      Authorization: "Bearer " + process.env.BEARER_TOKEN
+    }
   }, function (err, res, body) {
-    // TODO: Not async anymore
-    console.log('# Got', body.results.length, 'results');
+    console.log('Got', body.statuses.length, 'tweets');
 
-    async.each(body.results, function (tweet, eachTweetCb) {
-      if (tweet.entities && tweet.entities.urls) {
-        // TODO: Use an async queue here instead
-        async.each(tweet.entities.urls, function (url, eachUrlCb) {
-          elongate(url.expanded_url, function (err, expandedUrl) {
-            if (!stats[expandedUrl]) {
-              stats[expandedUrl] = 0;
-            }
-
-            stats[expandedUrl]++;
-
-            console.log(stats[expandedUrl], url.expanded_url, expandedUrl);
-
-            eachUrlCb();
-          });
-        }, function () {
-          eachTweetCb();
-        });
+    async.each(body.statuses, function (tweet, eachTweetCb) {
+      if (!tweet.entities || !tweet.entities.urls) {
+        return eachTweetCb();
       }
+
+      // TODO: Use an async queue here instead
+      async.each(tweet.entities.urls, function (url, eachUrlCb) {
+        elongate(url.expanded_url, function (err, expandedUrl) {
+          db.query("INSERT INTO urls (tweet_id, user_id, user_name, " +
+            "created_at, retweet_count, url) VALUES (?, ?, ?, ?, ?, ?)",
+            [tweet.id_str, tweet.user.id_str, tweet.user.screen_name,
+              tweet.created_at, tweet.retweet_count || 0, expandedUrl],
+            function (err) {
+            if (err && err.code !== 'ER_DUP_ENTRY') {
+              console.log('SQL error:', err);
+            } else if (!err) {
+              console.log('Added entry for', expandedUrl, 'from',
+                tweet.user.screen_name);
+            }
+          });
+
+          eachUrlCb();
+        });
+      }, function () {
+        eachTweetCb();
+      });
     }, function () {
-      if (body.next_page) {
-        getPage(body.next_page);
+      if (body.search_data && body.search_data.next_results) {
+        getPage(body.search_data.next_results);
       } else {
-        console.log('All done.');
-        console.log(JSON.stringify(stats, null, 2));
+        console.log('All done');
+
+        db.end();
       }
     });
   });
 }
 
-getPage();
+db.connect(function (err) {
+  if (err) {
+    console.log('Error connecting to database:', err);
+
+    process.exit(1);
+  }
+
+  getPage();
+});
